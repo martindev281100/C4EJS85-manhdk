b73c0b9da219ccd6a4f5986cbe5f300cfe4a03e8
Manh <43464766+martindevox2811@users.noreply.github.com>
2020-06-13 13:38:56 +0700

    13:38PM 6/13/2020

---
+++ Session 1/index.js
@@ -0,0 +1,45 @@
+console.log("Bài 1: ");
+console.log("A. Trong JS, xảy ra lỗi syntax khi: ");
+console.log("1. Biến được khởi tạo bắt đầu bằng số.");
+console.log("2. Biến được khởi tạo chưa những ký tự đặc biệt.");
+console.log("3. Biến được khởi tạo trùng tên với các từ khóa");
+console.log("Ví dụ:");
+console.log("VD1 Khai báo biến bắt đầu bằng số: let 1a");
+console.log("VD2 Biến khai báo có chứa ký tự đặc biệt: 123@");
+console.log("VD3 Biến trùng với lệnh hoặc keyword: let break")
+console.log("B. In JavaScript, how to check a variable data types?")
+console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến")
+console.log("Bài 2: ");
+console.log(" 1-b, 2-a, 3-e, 4-c")
+console.log("Bài 3: ");
+let message = 'Coding is great';
+console.log(message);
+let studentCount = 0;
+console.log("Student Count: " + studentCount);
+console.log("Bài 4:");
+message = 'Coding might not be easy, but still great';
+console.log(message);
+studentCount = 16;
+console.log("Student count: " + studentCount);
+let a = message.toLowerCase();
+console.log(a);
+studentCount++;
+console.log("StudentCount: " +studentCount);
+console.log("Bài 5: ");
+alert("You look btf today");
+console.log("Bài 6: ");
+let b = prompt("Hi buddy, Enter your name please?")
+alert("Hello " + b);
+console.log("Bài 7: ");
+let firstName = prompt("Enter your first name: ");
+let lastName = prompt("Enter your last name: ");
+alert( "Hi " + firstName + " " + lastName+ "!!!!");
+console.log("Bài 8: ");
+let lengthSquare = prompt("Enter length of the square: ");
+alert("The area square = " + lengthSquare*lengthSquare)
+console.log("Bài 9: ");
+let circleRad = prompt("Enter radius of the circle: ");
+alert("The area circle = " + circleRad*circleRad*3.14);
+console.log("Bài 10: ");
+let Celius = prompt("Enter the temperature in Celius");
+alert(Celius+"(C) = " + (Celius*1.8) + 32 + "(F)");
