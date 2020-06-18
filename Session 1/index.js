bbf055e6aa086d925d19916782bdb9fe780403df
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-06-18 21:22:24 +0700

    21:22PM 6/18/2020

--- Session 1/index.js
+++ Session 1/index.js
@@ -1,45 +1,93 @@
-console.log("Bài 1: ");
-console.log("A. Trong JS, xảy ra lỗi syntax khi: ");
-console.log("1. Biến được khởi tạo bắt đầu bằng số.");
-console.log("2. Biến được khởi tạo chưa những ký tự đặc biệt.");
-console.log("3. Biến được khởi tạo trùng tên với các từ khóa");
-console.log("Ví dụ:");
-console.log("VD1 Khai báo biến bắt đầu bằng số: let 1a");
-console.log("VD2 Biến khai báo có chứa ký tự đặc biệt: 123@");
-console.log("VD3 Biến trùng với lệnh hoặc keyword: let break")
-console.log("B. In JavaScript, how to check a variable data types?")
-console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến")
-console.log("Bài 2: ");
-console.log(" 1-b, 2-a, 3-e, 4-c")
-console.log("Bài 3: ");
-let message = 'Coding is great';
-console.log(message);
-let studentCount = 0;
-console.log("Student Count: " + studentCount);
-console.log("Bài 4:");
-message = 'Coding might not be easy, but still great';
-console.log(message);
-studentCount = 16;
-console.log("Student count: " + studentCount);
-let a = message.toLowerCase();
-console.log(a);
-studentCount++;
-console.log("StudentCount: " +studentCount);
-console.log("Bài 5: ");
-alert("You look btf today");
-console.log("Bài 6: ");
-let b = prompt("Hi buddy, Enter your name please?")
-alert("Hello " + b);
-console.log("Bài 7: ");
-let firstName = prompt("Enter your first name: ");
-let lastName = prompt("Enter your last name: ");
-alert( "Hi " + firstName + " " + lastName+ "!!!!");
-console.log("Bài 8: ");
-let lengthSquare = prompt("Enter length of the square: ");
-alert("The area square = " + lengthSquare*lengthSquare)
-console.log("Bài 9: ");
-let circleRad = prompt("Enter radius of the circle: ");
-alert("The area circle = " + circleRad*circleRad*3.14);
-console.log("Bài 10: ");
-let Celius = prompt("Enter the temperature in Celius");
-alert(Celius+"(C) = " + (Celius*1.8) + 32 + "(F)");
+// console.log("Bài 1: ");
+// console.log("A. Trong JS, xảy ra lỗi syntax khi: ");
+// console.log("1. Biến được khởi tạo bắt đầu bằng số.");
+// console.log("2. Biến được khởi tạo chưa những ký tự đặc biệt.");
+// console.log("3. Biến được khởi tạo trùng tên với các từ khóa");
+// console.log("Ví dụ:");
+// console.log("VD1 Khai báo biến bắt đầu bằng số: let 1a");
+// console.log("VD2 Biến khai báo có chứa ký tự đặc biệt: 123@");
+// console.log("VD3 Biến trùng với lệnh hoặc keyword: let break");
+// console.log("B. In JavaScript, how to check a variable data types?");
+// console.log("Dùng lệnh typeof để xem kiểu dữ liệu của biến");
+// console.log("Bài 2: ");
+// console.log(" 1-b, 2-a, 3-e, 4-c")
+// console.log("Bài 3: ");
+// let message = 'Coding is great';
+// console.log(message);
+// let studentCount = 0;
+// console.log("Student Count: " + studentCount);
+// console.log("Bài 4:");
+// message = 'Coding might not be easy, but still great';
+// console.log(message);
+// studentCount++;
+// console.log("StudentCount: " +studentCount);
+// console.log("Bài 5: ");
+// alert("You look btf today");
+// console.log("Bài 6: ");
+// let b = prompt("Hi buddy, Enter your name please?")
+// alert("Hello " + b);
+// console.log("Bài 7: ");
+// let firstName = prompt("Enter your first name: ");
+// let lastName = prompt("Enter your last name: ");
+// alert( "Hi " + firstName + " " + lastName+ "!!!!");
+// console.log("Bài 8: ");
+// let lengthSquare = prompt("Enter length of the square: ");
+// alert("The area square = " + lengthSquare*lengthSquare)
+// console.log("Bài 9: ");
+// let circleRad = prompt("Enter radius of the circle: ");
+// alert("The area circle = " + circleRad*circleRad*3.14);
+// console.log("Bài 10: ");
+// let Celius = prompt("Enter the temperature in Celius");
+// alert(Celius+"(C) = " + (Celius*1.8) + 32 + "(F)");
+// let a= Math.round(4.7);    // returns 5
+// let b= Math.round(4.4);    // returns 4
+// console.log("a= "+a);
+// console.log("b= "+b);
+// let c= Math.pow(9,5);
+// console.log("c= "+c);
+//  a= Math.abs(-342*3421);
+// console.log("a= "+a);
+// console.log("Random number between 0 and 1: "+Math.random());
+// console.log("PI: "+Math.PI)
+// console.log(a||b);
+// let a = 100000000;
+// {
+//     var b= 19348;
+//     console.log(a);
+// }
+// console.log (b);
+// const c = 100;
+// c= 12;
+// console.log(c);
+// let MYFAV;
+// let n = String(45);
+// console.log(`${n+n}`);
+//Bài 1: 
+//A. Bị lỗi vì khai báo trùng lặp biến x
+//B. x=10 vì được cập nhật biến từ 5 thành 10
+//C. break 
+// let n =9;
+// console.log(`${n}`);
+let b = 0
+while(b<100)
+{
+   b++;
+   console.log(b);
+}
+
+// for (let x=0; x<100; x++)
+// {
+// console.log(x);
+// }
+// let x=0;
+// x = prompt("Enter begin number: ")
+// a = prompt("Enter end number: ")
+// while(x<=a)
+// {
+// x++;
+// console.log(x);
+// }
+let x =0;
+do{
+console.log(x++)
+}while(x <10)
